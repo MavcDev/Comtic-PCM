a9355ada706e7d6b956e70b0e3abefa435eed4fd
Mavc <mavc1220@gmail.com>
2019-12-06 16:45:19 -0500

    Actualizacion de home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { PlayPageComponent } from './page/play-page/play-page.component';
+import { HomePageComponent } from './page/home-page/home-page.component';
 
 const routes: Routes = [
   {path: 'play', component: PlayPageComponent},
-  {path: '', redirectTo: 'play', pathMatch:  'full'}
+  {path: 'home', component: HomePageComponent},
+  {path: '', redirectTo: 'home', pathMatch:  'full'}
 ];
 
 @NgModule({
